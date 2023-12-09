33b775832dde4adecbe1576e0d59439578f6cc3b
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-12-09 21:06:44 +0200

    Tweaking
    Updates and improvements from the database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -14,35 +14,37 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,      
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({ email: session.user.email });
-
-        session.user.id = sessionUser.id.toString();
-
-        return session;
-    },
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
-
-            // Check if a user already exists
-            const userExists = await User.findOne({ email: profile.email });
-            
-            // If not, create a new user
-            if (!userExists) {
-                await User.create({ 
-                    email: profile.email, 
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                });
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({ email: session.user.email });
+    
+            session.user.id = sessionUser.id.toString();
+    
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
+    
+                // Check if a user already exists
+                const userExists = await User.findOne({ email: profile.email });
+                
+                // If not, create a new user
+                if (!userExists) {
+                    await User.create({ 
+                        email: profile.email, 
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    });
+                }
+    
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
             }
-
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
         }
-    }
+    },
 })
 
 export { handler as GET, handler as POST};
