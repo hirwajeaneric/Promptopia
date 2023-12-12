dde95add965bfe52b3bb7d45bdda69b57be32bc9
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-12-12 10:36:13 +0200

    Copy to clipboard functionality

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,7 +6,13 @@ import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-  const [copied, setCopied] = useState("")
+  const [copied, setCopied] = useState("");
+
+  const handleCopy = () => { 
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 3000);
+  }
 
   return (
     <div className='prompt_card'>
@@ -29,7 +35,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           </div>
         </div>
 
-        <div className='copy_btn' onClick={() => {}}>
+        <div className='copy_btn' onClick={() => {handleCopy()}}>
           <Image 
             src={copied === post.prompt
               ? '/assets/icons/tick.svg'
